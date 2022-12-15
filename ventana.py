372f59965cc68609739f258cdab3a90aafc2ffd2
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-12-15 18:07:51 -0300

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -1,20 +1,20 @@
-import pygame
-import sys
-
+import pygame, sys
+from Dibujos import *
 pygame.init()
-pygame.font.init()
-pygame.mixer.init()
-color_blanco = (255, 255, 255)
 
-ventana = pygame.display.set_mode((1000,700))
-ventana.fill(color_blanco)
-pygame.display.set_caption("Memorizan2")
+#Fondo imagen
+background = pygame.image.load("memorizan2.png").convert()
 
-
+#Ventana y para cerrar
+size = (1000,700)
+screen = pygame.display.set_mode(size)
 while True:
-    for event in pygame.event.get():    
-        if event.type == pygame.QUIT:   
-            pygame.quit()              
-            sys.exit()              
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    pygame.display.flip()         
+    dibujo = Dibujo(screen)
+    dibujo.dibujar_rectangulos()                                #llama a la clase que crea los cuadrados de fondo
+    screen.blit(background, [250,100])                          #para mostrar la imagen         
+    pygame.display.set_caption("Memoriza2")                     #Para mostrar nombre del juego 
+    pygame.display.flip()                                       #Para actualizar la pantalla
