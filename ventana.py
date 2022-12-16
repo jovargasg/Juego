7562a7b8a6bebc92ed6595e56c008cf51d37b819
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-12-16 10:03:35 -0300

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -8,12 +8,12 @@ background = pygame.image.load("memorizan2.png").convert()
 #Ventana y para cerrar
 size = (1000,700)
 screen = pygame.display.set_mode(size)
-while True:
+while True:                                                     #Bucle para cerrar
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    dibujo = Dibujo(screen)
+    dibujo = Dibujo(screen)                                     
     dibujo.dibujar_rectangulos()                                #llama a la clase que crea los cuadrados de fondo
     screen.blit(background, [250,100])                          #para mostrar la imagen         
     pygame.display.set_caption("Memoriza2")                     #Para mostrar nombre del juego 
