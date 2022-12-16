08e1e40a5829bffaac7abb28ffe1b01e45fb683b
jovargasg <116354817+jovargasg@users.noreply.github.com>
2022-12-16 10:06:46 -0300

    Update ventana.py

--- ventana.py
+++ ventana.py
@@ -1,17 +1,15 @@
-import pygame, sys
-from Dibujos import *
-pygame.init()
+import pygame, sys                                              #Importar librerias
+from Dibujos import *                                           #Importar la clase Dibujos para los cuadros de fondo de diseño
+pygame.init()                                                   #Iniciar pygame
 
-#Fondo imagen
-background = pygame.image.load("memorizan2.png").convert()
+background = pygame.image.load("memorizan2.png").convert()      #Imagen sobre puesta
 
-#Ventana y para cerrar
-size = (1000,700)
-screen = pygame.display.set_mode(size)
-while True:                                                     #Bucle para cerrar
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+size = (1000,700)                                               #Definir tamaño de la ventana
+screen = pygame.display.set_mode(size)                          #
+while True:                                                     #Bucle para cerrar la ventana
+    for event in pygame.event.get():                            #Evento de pygame
+        if event.type == pygame.QUIT:                           #Si el evento es apretar la "x" de la ventana,
+            sys.exit()                                          #Esta ventana se cierra
 
     dibujo = Dibujo(screen)                                     
     dibujo.dibujar_rectangulos()                                #llama a la clase que crea los cuadrados de fondo
